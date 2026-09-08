951fe6fa10c3fdcfc040db79cf22122066d3eead
DaviFs <davifernandes0024509@gmail.com>
2026-09-08 17:50:30 -0400

    Opt-in ExperimentalMaterial3Api for TopAppBar/FilterChip in AnimeDetailScreen

--- app/src/main/java/com/akiro/anime/ui/screens/detail/AnimeDetailScreen.kt
+++ app/src/main/java/com/akiro/anime/ui/screens/detail/AnimeDetailScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -27,6 +28,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import com.akiro.anime.data.model.Episode
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AnimeDetailScreen(
     animeId: String,
