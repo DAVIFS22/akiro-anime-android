daf8509ed7e1f6bebae47e998030c8d6da36ad21
DaviFs <davifernandes0024509@gmail.com>
2026-09-08 22:13:06 -0400

    Delete app directory

--- app/proguard-rules.pro
+++
@@ -1,4 +0,0 @@
-# Add project specific ProGuard rules here.
-
-# libtorrent4j JNI entry points
--keep class org.libtorrent4j.swig.libtorrent_jni { *; }
--- app/src/main/java/com/akiro/anime/AkiroApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.akiro.anime
-
-import android.app.Application
-
-class AkiroApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
